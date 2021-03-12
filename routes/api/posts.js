9f4fc70d7c0b591dbe9a642c91721216be7341da
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-03-12 14:59:16 +0100

    Created login route where we get token as a response wich we can use for protected routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
 
 // @route     GET api/posts
 // @desc      Test route
 // @access    Public
-router.get("/", (req, res) => res.send("Posts route"));
+router.get("/", (req, res) => {
+   var a = 5
+
+   function outer() {
+      console.log(a)
+      var a = 4
+   }
+   outer()
+
+    res.send("Posts route")
+})
 
 module.exports = router;
