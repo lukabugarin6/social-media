280e30cab1fa6e7b9a88c7507f7e5c4ba0771841
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-03-16 09:31:40 +0100

    created route for adding posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route     GET api/posts
-// @desc      Test route
-// @access    Public
-router.get("/", (req, res) => {
-   var a = 5
+// @route     POST api/posts
+// @desc      Create a post
+// @access    Private
+router.post("/", [auth, [
+   check('text', 'Text is required').not().isEmpty()
+]],async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+     return res.status(400).json({errors: errors.array()});
+  }
 
-   function outer() {
-      console.log(a)
-      var a = 4
-   }
-   outer()
+  try {
+     const user = await User.findById(req.user.id).select('-password');
+   
+     const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+     })
+     
+     const post = await newPost.save();
 
-    res.send("Posts route")
+     res.json(post);
+
+  } catch (error) {
+     console.error(error.message);
+     res.status(500).send('Server Error');
+  }
+
 })
 
 module.exports = router;
