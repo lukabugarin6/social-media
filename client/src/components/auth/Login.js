ae6b92261625d42f265b0feaf9f2803219d4fb6e
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-03-22 10:31:02 +0100

    user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from 'axios';
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
+import axios from "axios";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -15,46 +18,75 @@ const Login = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log('success');
+    login(email, password);
+  };
+
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
   }
 
   return (
     <div className="landing flex justify-center items-center">
-      <div className='form-card rounded-lg p-8 text-white' style={{backgroundColor:"rgba(255,255,255,0.1)"}}>
-      <h1 className='text-3xl text-center font-medium'>Sign Into Your Account</h1>
-      <form onSubmit={e => submitHandler(e)}>
-      <div className='flex flex-col gap-y-6 pt-12'> 
-
-        <div className='flex flex-col'>
-          <input
-            type="email"
-            placeholder="Email Address"
-            value={email}
-            className='bg-quaternary text-white placeholder-white pl-3.5 py-3.5 rounded-lg'
-            onChange={(e) => changeHandler(e)}
-            name="email"
-          />
-        </div>
-        <div className='flex flex-col'>
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            className='bg-quaternary text-white placeholder-white pl-3.5 py-3.5 rounded-lg'
-            value={password}
-            onChange={(e) => changeHandler(e)}
-            minLength="6"
-          />
-        </div>
-        <button className='button text-white origin-left rounded-lg font-medium py-3.5 w-24 text-center cursor-pointer' type="submit">Login</button>
-      </div>
-      </form>
-      <p className='text-sm mt-4'>
-        Dont have an account? <Link to="/register" className='redirect-btn relative transition-all duration-300 text-quinary font-semibold hover:text-primary'>Register</Link>
-      </p>
+      <div
+        className="form-card rounded-lg p-8 text-white"
+        style={{ backgroundColor: "rgba(255,255,255,0.1)" }}
+      >
+        <h1 className="text-3xl text-center font-medium">
+          Sign Into Your Account
+        </h1>
+        <form onSubmit={(e) => submitHandler(e)}>
+          <div className="flex flex-col gap-y-6 pt-12">
+            <div className="flex flex-col">
+              <input
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                className="bg-quaternary text-white placeholder-white pl-3.5 py-3.5 rounded-lg"
+                onChange={(e) => changeHandler(e)}
+                name="email"
+              />
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                className="bg-quaternary text-white placeholder-white pl-3.5 py-3.5 rounded-lg"
+                value={password}
+                onChange={(e) => changeHandler(e)}
+                minLength="6"
+              />
+            </div>
+            <button
+              className="button text-white origin-left rounded-lg font-medium py-3.5 w-24 text-center cursor-pointer"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="text-sm mt-4">
+          Dont have an account?{" "}
+          <Link
+            to="/register"
+            className="redirect-btn relative transition-all duration-300 text-quinary font-semibold hover:text-primary"
+          >
+            Register
+          </Link>
+        </p>
       </div>
     </div>
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect  } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 import Alert from "../layout/Alert";
 // import axios from 'axios';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -45,6 +45,11 @@ const Register = ({ setAlert, register }) => {
       // }
     }
   };
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+      return <Redirect to="/dashboard" />;
+    }
 
   return (
     <div className="landing flex justify-center items-center">
@@ -127,6 +132,11 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
